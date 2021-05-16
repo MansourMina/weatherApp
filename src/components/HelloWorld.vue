b15c0e80ca8964c9c9090d0a354790309c112395
Mina Mansour <mansourmina10@gmail.com>
2021-05-16 22:39:39 +0200

    safety precautions taken

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,10 +10,7 @@
             <h1 class="large-font mr-3">{{ currentWeather }}</h1>
             <div class="d-flex flex-column mr-3">
               <h2 class="mt-3 mb-0">{{ countryName }}</h2>
-              <small>
-                {{ timeNow() }} - {{ arrayOfWeekdays[new Date().getDay()] }},
-                {{ getDate() }}</small
-              >
+              <small> {{ timeofCity }}</small>
             </div>
             <div class="d-flex flex-column text-center">
               <h3 class="fa fa-sun-o mt-4"></h3>
@@ -65,7 +62,7 @@
         <div class="card1 col-lg-8 col-md-7">
           <div class="d-flex flex-column text-center">
             <h3 class="fa fa-sun-o mt-4"></h3>
-            <p class="display-5">Please search for a city</p>
+            <p class="display-5">{{ message }}</p>
           </div>
         </div>
         <div class="card2 col-lg-4 col-md-5">
@@ -119,6 +116,8 @@ export default {
   data() {
     return {
       currentWeather: null,
+      openweatherKey: process.env.VUE_APP_OW_KEY,
+      timeZoneKey: process.env.VUE_APP_TZ_KEY,
       countryName: null,
       imageWeather: null,
       tmax: null,
@@ -131,7 +130,9 @@ export default {
       searchCountry: '',
       lat: null,
       long: null,
+      message: 'Please search for a city',
       description: null,
+      timeofCity: null,
       clickMethod: false,
       arrayOfWeekdays: [
         'Sunday',
@@ -163,62 +164,113 @@ export default {
       navigator.geolocation.getCurrentPosition((position) => {
         this.long = position.coords.longitude;
         this.lat = position.coords.latitude;
-        console.log(this.long);
+        // console.log(this.long);
       });
     }
   },
+
   methods: {
     async getWeather() {
-      const api = `https://api.openweathermap.org/data/2.5/weather?q=${this.searchCountry}&appid=52293b43835f810dfe6fc255311d9794`;
+      const api = `https://api.openweathermap.org/data/2.5/weather?q=${this.searchCountry}&appid=${this.openweatherKey}`;
+
+      const response = await fetch(api);
+      if (!response.ok) {
+        this.clickMethod = false;
+        this.message = 'City not found';
+      } else {
+        const json = await response.json();
+        // console.log(json);
+        const temperature = Math.ceil(json.main.temp - 273.15) + '°';
+        let dateTime = new Intl.DateTimeFormat(
+          [],
+          this.getTime(
+            await this.getTimeZone(json.coord.lon, json.coord.lat),
+            'dateTime',
+          ),
+        );
+        let sunriseSunset = new Intl.DateTimeFormat(
+          [],
+          this.getTime(
+            await this.getTimeZone(json.coord.lon, json.coord.lat),
+            'sunriseSunset',
+          ),
+        );
+        // let regionNames = new Intl.DisplayNames(['en'], { type: 'region' });
+        this.countryName = json.name;
+        this.currentWeather = temperature;
+        this.description = json.weather[0].main;
+        let weatherIcon = json.weather[0].icon;
+        this.feelsLike = Math.ceil(json.main.feels_like - 273.15) + '°';
+        this.tmax = Math.ceil(json.main.temp_max - 273.15) + '°';
+        this.tmin = Math.ceil(json.main.temp_min - 273.15) + '°';
+        this.sunrise = sunriseSunset.format(new Date(json.sys.sunrise * 1000));
+        this.sunset = sunriseSunset.format(new Date(json.sys.sunset * 1000));
+        this.humidity = json.main.humidity + '%';
+        this.imageWeather = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+        this.clickMethod = true;
+
+        this.timeofCity = dateTime.format(new Date());
+        // console.log(this.convertInTime(timeofCity));
+
+        // console.log(this.description);
+      }
+    },
+    // timeNow() {
+    //   var d = new Date();
+    //   let hours = d.getHours();
+    //   let minutes = d.getMinutes();
+    //   if (minutes < 10 && hours < 10)
+    //     return `0${d.getHours()}:0${d.getMinutes()}`;
+    //   if (hours < 10) return `0${d.getHours()}:${d.getMinutes()}`;
+    //   if (minutes < 10) return `${d.getHours()}:0${d.getMinutes()}`;
+    //   return `${d.getHours()}:${d.getMinutes()}`;
+    // },
+    // getDate() {
+    //   var dateObj = new Date();
+    //   var month = dateObj.getMonth() + 1; //months from 1-12.
+    //   var day = dateObj.getDate();
+    //   var year = dateObj.getFullYear();
+    //   return `${day} ${this.arrayOfMonths[month - 1]} '${year}`;
+    // },
+    convertInTime(d) {
+      let hours = d.getHours() - 6;
+      let minutes = d.getMinutes();
+      if (minutes < 10 && hours < 10)
+        return `0${d.getHours()}:0${d.getMinutes()}`;
+      if (hours < 10) return `0${d.getHours()}:${d.getMinutes()}`;
+      if (minutes < 10) return `${d.getHours()}:0${d.getMinutes()}`;
+      return `${d.getHours()}:${d.getMinutes()}`;
+    },
+    async getTimeZone(long, lat) {
+      const api = `http://api.timezonedb.com/v2.1/get-time-zone?key=${this.timeZoneKey}&format=json&by=position&lat=${lat}&lng=${long}`;
 
       const response = await fetch(api);
       if (!response.ok) {
         console.log('etwas schiefgelaufen');
       }
-
       const json = await response.json();
-      console.log(json);
-      const temperature = Math.ceil(json.main.temp - 273.15) + '°';
-      // let regionNames = new Intl.DisplayNames(['en'], { type: 'region' });
-      this.countryName = json.name;
-      this.currentWeather = temperature;
-      this.description = json.weather[0].main;
-      let weatherIcon = json.weather[0].icon;
-      this.feelsLike = Math.ceil(json.main.feels_like - 273.15) + '°';
-      this.tmax = Math.ceil(json.main.temp_max - 273.15) + '°';
-      this.tmin = Math.ceil(json.main.temp_min - 273.15) + '°';
-      this.sunrise = this.convertInTime(new Date(json.sys.sunrise * 1000));
-      this.sunset = this.convertInTime(new Date(json.sys.sunset * 1000));
-      this.humidity = json.main.humidity + '%';
-      this.imageWeather = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
-      this.clickMethod = true;
-      console.log(this.description);
+      return json.zoneName;
     },
-    timeNow() {
-      var d = new Date();
-      let hours = d.getHours();
-      let minutes = d.getMinutes();
-      if (minutes < 10 && hours < 10)
-        return `0${d.getHours()}:0${d.getMinutes()}`;
-      if (hours < 10) return `0${d.getHours()}:${d.getMinutes()}`;
-      if (minutes < 10) return `${d.getHours()}:0${d.getMinutes()}`;
-      return `${d.getHours()}:${d.getMinutes()}`;
-    },
-    getDate() {
-      var dateObj = new Date();
-      var month = dateObj.getMonth() + 1; //months from 1-12.
-      var day = dateObj.getDate();
-      var year = dateObj.getFullYear();
-      return `${day} ${this.arrayOfMonths[month - 1]} '${year}`;
-    },
-    convertInTime(d) {
-      let hours = d.getHours();
-      let minutes = d.getMinutes();
-      if (minutes < 10 && hours < 10)
-        return `0${d.getHours()}:0${d.getMinutes()}`;
-      if (hours < 10) return `0${d.getHours()}:${d.getMinutes()}`;
-      if (minutes < 10) return `${d.getHours()}:0${d.getMinutes()}`;
-      return `${d.getHours()}:${d.getMinutes()}`;
+    getTime(zoneCity, type) {
+      if (type == 'dateTime') {
+        let options = {
+          timeZone: zoneCity,
+          month: 'numeric',
+          day: 'numeric',
+          year: 'numeric',
+          hour: 'numeric',
+          minute: 'numeric',
+        };
+        return options;
+      }
+      if (type == 'sunriseSunset') {
+        let options = {
+          timeZone: zoneCity,
+          hour: 'numeric',
+          minute: 'numeric',
+        };
+        return options;
+      }
     },
   },
 };
